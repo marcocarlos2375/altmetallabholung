88ca997ba2893329fd1b0974396d12c0c1b78d76
marcocarlos2375 <80224312+marcocarlos2375@users.noreply.github.com>
2023-04-01 18:05:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ buttons.addEventListener("click", function(){
    buttons.classList.toggle("toggled")
    document.getElementsByTagName("body")[0].classList.toggle("ast-main-header-nav-open")
 });
+
+
+var items = document.getElementsByClassName("elementor-toggle-item")[0]
+items.addEventListener("click", function(){
+    console.log("click");
+   var x = items.getElementsByClassName("elementor-tab-title")[0]
+   x.classList.add("elementor-active")
+   var y =items.getElementsByClassName("elementor-tab-content")[0]
+   y.style.display="block";
+});
